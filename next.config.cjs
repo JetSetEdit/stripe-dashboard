ef5ae435fc9833ab9c7f835eda2ab768a6a5cb93
User <user@example.com>
2025-04-01 14:04:29 +1100

    Fix environment variable names in Next.js config

--- next.config.cjs
+++ next.config.cjs
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
     STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
   },
 };
